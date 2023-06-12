648b45efa5f92f2e80c5416d3d4f8757b3d59a63
meanlightk <meanlightk@gmail.com>
2023-06-12 14:27:52 +0900

    AOP 이해

--- BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -7,38 +7,49 @@ import org.springframework.stereotype.Service;
 
 import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.common.Log4jAdvice;
 
 @Service("boardService")
 public class BoardServiceImpl implements BoardService {
 	@Autowired
 	private BoardDAO boardDAO;
+	private Log4jAdvice log;
+
+	public BoardServiceImpl() {
+		log = new Log4jAdvice();
+	}
 
 	@Override
 	public void insertBoard(BoardVO vo) {
+		log.pringLogging();
 		boardDAO.insertBoard(vo);
 
 	}
 
 	@Override
 	public void updateBoard(BoardVO vo) {
+		log.pringLogging();
 		boardDAO.updateBoard(vo);
 
 	}
 
 	@Override
 	public void deleteBoard(BoardVO vo) {
+		log.pringLogging();
 		boardDAO.deleteBoard(vo);
 
 	}
 
 	@Override
 	public void getBoard(BoardVO vo) {
+		log.pringLogging();
 		boardDAO.getBoard(vo);
 
 	}
 
 	@Override
 	public List<BoardVO> getBoardList(BoardVO vo) {
+		log.pringLogging();
 		return boardDAO.getBoardList(vo);
 	}
 }

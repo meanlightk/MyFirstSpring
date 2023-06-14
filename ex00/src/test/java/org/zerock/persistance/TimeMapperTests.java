0b2c8e88f65ac902214b761282aec18a28284b0d
meanlightk <meanlightk@gmail.com>
2023-06-14 16:10:56 +0900

    MyBatis 연결

---
+++ ex00/src/test/java/org/zerock/persistance/TimeMapperTests.java
@@ -0,0 +1,30 @@
+package org.zerock.persistance;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.extern.log4j.Log4j;
+import mapper.TimeMapper;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { org.zerock.config.RootConfig.class })
+@Log4j
+public class TimeMapperTests {
+	@Autowired
+	private TimeMapper timeMapper;
+
+	@Test
+	public void testGetTime() {
+		log.info(timeMapper.getClass().getName());
+		log.info(timeMapper.getTime());
+	}
+
+	public void testGetTime2() {
+		log.info("getTime2");
+		log.info(timeMapper.getTime2());
+	}
+
+}

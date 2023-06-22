e9ae83c1e69fc3c967c5bf61e4ca162d53e784b9
meanlightk <meanlightk@gmail.com>
2023-06-22 13:34:06 +0900

    목록처리&테스트

--- ex02/src/main/java/org/zerock/controller/BoardController.java
+++ ex02/src/main/java/org/zerock/controller/BoardController.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.zerock.service.BoardService;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
 @RequestMapping("/board/*")
+@AllArgsConstructor
 public class BoardController {
 
 	private BoardService service;
--- ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
+++ ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -1,12 +1,11 @@
 package org.zerock.controller;
 
-import org.aspectj.lang.annotation.Before;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -17,7 +16,6 @@ import lombok.extern.log4j.Log4j;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "file:src/main/webapp/WEB-INF/spring/root-context.xml",
 						"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml" })
-@WebAppConfiguration
 @Log4j
 public class BoardControllerTests {
 
@@ -33,10 +31,7 @@ public class BoardControllerTests {
 
 	@Test
 	public void testList() throws Exception {
-		log.info(mockMvc.perform(MockMvcRequestBuilders.get("/board/list"))
-				.andReturn()
-				.getModelAndView()
-				.getModelMap());
+		log.info(
+				mockMvc.perform(MockMvcRequestBuilders.get("/board/list")).andReturn().getModelAndView().getModelMap());
 	}
-
 }

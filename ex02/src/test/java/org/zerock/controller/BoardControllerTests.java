ce342dc3a84b539df767c467e4a69555b5ad5876
meanlightk <meanlightk@gmail.com>
2023-06-22 13:53:33 +0900

    등록처리&테스트

--- ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
+++ ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -14,6 +15,10 @@ import org.springframework.web.context.WebApplicationContext;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+
+// Test for Controller
+@WebAppConfiguration
+
 @ContextConfiguration({ "file:src/main/webapp/WEB-INF/spring/root-context.xml",
 						"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml" })
 @Log4j
@@ -32,6 +37,24 @@ public class BoardControllerTests {
 	@Test
 	public void testList() throws Exception {
 		log.info(
-				mockMvc.perform(MockMvcRequestBuilders.get("/board/list")).andReturn().getModelAndView().getModelMap());
+				mockMvc.perform(
+						MockMvcRequestBuilders.get("/board/list"))
+						.andReturn()
+						.getModelAndView()
+						.getModelMap());
+	}
+	
+	
+	@Test
+	public void testRegister() throws Exception {
+		String resultPage = mockMvc.perform(
+						MockMvcRequestBuilders.post("/board/register")
+						.param("title","레지스터 테스트 새 글 제목")
+						.param("content","레지스터 테스트 새 글 내용")
+						.param("writer","레지스터 user00")
+				).andReturn().getModelAndView().getViewName();
+		
+		log.info(resultPage);
+				
 	}
 }

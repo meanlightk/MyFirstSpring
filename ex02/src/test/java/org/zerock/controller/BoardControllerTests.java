c2c669fcd73e3ed5f779f340a0ac6acef2b903ba
meanlightk <meanlightk@gmail.com>
2023-06-29 15:16:36 +0900

    MyBatis와 스프링에서 페이징 처리

--- ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
+++ ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -89,4 +89,12 @@ public class BoardControllerTests {
 		log.info(resultPage);
 	}
 	
+	@Test
+	public void testListPaging() throws Exception {
+		log.info(mockMvc.perform(MockMvcRequestBuilders.post("/board/list")
+				.param("pageNum","2")
+				.param("amount","50")
+				).andReturn().getModelAndView().getModelMap());
+	}
+	
 }
--- ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -1,11 +1,14 @@
 package org.zerock.mapper;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 
 import lombok.extern.log4j.Log4j;
 
@@ -71,11 +74,24 @@ public class BoardMapperTests {
 		board.setBno(5L);
 		board.setTitle("수정된 제목");
 		board.setContent("수정된 내용");
-	  //board.setWriter("user00");
+		// board.setWriter("user00");
 
 		int count = mapper.update(board);
 		log.info("UPDATE COUNT: " + count);
 
+	}
+
+	@Test
+	public void testPaging() {
+		
+		Criteria cri = new Criteria();
+		// 10개씩 3페이지
+		cri.setPageNum(3);
+		cri.setAmount(10);
+		
+		List<BoardVO> list = mapper.getListWithPaging(cri);
+		
+		list.forEach(board -> log.info(board.getBno()));
 	}
 
 }
